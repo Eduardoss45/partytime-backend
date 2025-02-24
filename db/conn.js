c0a12d42211ca61def1c02c474492470a72c4071
Eduardo <temporariac432@gmail.com>
2025-02-24 10:57:43 -0300

    Problemas na recepção de dados

--- db/conn.js
+++ db/conn.js
@@ -5,7 +5,7 @@ async function main() {
     mongoose.set("strictQuery", true);
 
     await mongoose.connect(
-      "mongodb://eduardo:I03cLnE5gnoV8JPu@ac-c0pfbt6-shard-00-00.xlmhjcx.mongodb.net:27017,ac-c0pfbt6-shard-00-01.xlmhjcx.mongodb.net:27017,ac-c0pfbt6-shard-00-02.xlmhjcx.mongodb.net:27017/?ssl=true&replicaSet=atlas-xw5ill-shard-0&authSource=admin&retryWrites=true&w=majority&appName=Cluster0"
+      "mongodb://eduardo:ysK0oA6asj2kFiS0@ac-c0pfbt6-shard-00-00.xlmhjcx.mongodb.net:27017,ac-c0pfbt6-shard-00-01.xlmhjcx.mongodb.net:27017,ac-c0pfbt6-shard-00-02.xlmhjcx.mongodb.net:27017/?ssl=true&replicaSet=atlas-xw5ill-shard-0&authSource=admin&retryWrites=true&w=majority&appName=Cluster0"
     );
 
     console.log("Conectado!");
